95ef32eee656d089fc1abf21d34e0ad6ff17fe85
Chatbot123 <42810864+Chatbot123@users.noreply.github.com>
2020-01-08 14:46:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,16 +14,58 @@ app.post('/CheckAccountStatus', (req, res) =>
 {
 	  //console.log(JSON.stringify(req.body));
 	  //console.log(req.body.conversation.memory.intent_name.slug);
-	  speech = " Thanks for contacting us."
+	  //speech = " Thanks for contacting us."
 	//var speech;
-	//if(req.body.conversation.memory.intent_name.slug=="unlockaccount-personal")
-	//{	
+	if(req.body.conversation.memory.intent_name.slug=="unlockaccount-personal")
+	{	
 		//speech = " Thanks for contacting us. checking for personal account status"
-	
-			speech = speech.replace(/  +/g, ' ');
-			//console.log(speech);//----------------------------------------------
-			//console.log(speech);
-			var reply = [{
+		var csrfToken;
+		var url = "https://sapmobile-gwx.centurylink.com/sap/opu/odata/sap/ZUSER_MAINT_OPRS_CHATBOT_SRV/UserStatusCheckSet(Aduser='praveen.varriam@centurylink.com',PwdStatusCheckFlag='X',UserLockCheckFlag='X')/?$format=json";
+		//console.log(url);
+		var options = { 
+				method: 'GET',
+	  			
+	 				//'https://sapmobile-gwx.centurylink.com/sap/opu/odata/sap/ZUSER_MAINT_OPRS_CHATBOT_SRV;o=ERP_700/ADUserDetailsSet?$filter=(Aduser%20eq%20%27praveen.varriam@centurylink.com%27)&$format=json',
+				headers: 
+	   				{ 
+					     'Host': 'sapmobile-gwx.centurylink.com',
+					     'Authorization': 'Basic Q0hBVEJPVF9URVNUOlRlc3QxMjM0',
+					     'content-type':'application/json',
+					     'x-csrf-token':'Fetch' 
+					} 
+				};
+
+
+		requestify.request(url,options).then(function(response)
+		{
+		  	var csrfToken;
+			// console.log(response);
+			//console.log("status code"+response.code);
+			
+	  		if (response.code == 200) 
+			{   
+				csrfToken = response.headers['x-csrf-token'];
+				//console.log("csrf token" + csrfToken);
+				var res = JSON.parse(response.body);
+				var statusDetail = res.d.AccountStatus;               
+	 		}
+	 		//console.log("statusDetail" + statusDetail);
+			
+	 		if (statusDetail == 'ACCOUNT_NOT_LOCKED AND PASSWORD_NOT_EXPIRED')
+	 		{
+	   			 speech = "Your account is already unlocked!";
+	 		}
+	 		else if(statusDetail == 'ACCOUNT_SUSPENDED_BY_ADMIN AND PASSWORD_NOT_EXPIRED')
+	 		{
+			  // builder.Prompts.choice(session, "Your account has been locked by admin. I can help you get it unlocked, do you want me to proceed ?","Yes|No",{listStyle:3});
+	   			 speech = "Your account has been locked by admin. I can help you get it unlocked, do you want me to proceed ?";
+	 		}
+			
+			
+			
+						    
+		});
+		var reply = [{
 				type: 'text',
 				content: speech
 				}];
@@ -31,15 +73,8 @@ app.post('/CheckAccountStatus', (req, res) =>
 			res.status(200).json({
     					replies: reply
   				});
-			//res.status(200).json({	replies: reply	});
-			//console.log(res.body);
-			
-			
-						    
 		
-		
-		
-	//}
+	}
 	//----------------------------------------------
 });
 
