b45507d38da411c4c2cbc87acdc3f2293e2a6070
Chatbot123 <42810864+Chatbot123@users.noreply.github.com>
2020-01-08 15:00:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,14 +8,14 @@ app.use(bodyParser.json())
 // Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`App is listening on port ${PORT}`));
-
+var speech;
 //code for creating servicenow incident
 app.post('/CheckAccountStatus', (req, res) => 
 {
 	  //console.log(JSON.stringify(req.body));
 	  //console.log(req.body.conversation.memory.intent_name.slug);
 	  //speech = " Thanks for contacting us."
-	var speech;
+	//var speech;
 	//if(req.body.conversation.memory.intent_name.slug=="unlockaccount-personal")
 	//{	
 		//speech = " Thanks for contacting us. checking for personal account status"
